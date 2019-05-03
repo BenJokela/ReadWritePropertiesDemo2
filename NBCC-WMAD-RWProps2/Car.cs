fd63bcae6f61f8908676c2329511780073afc932
BenJokela <benjokela@gmail.com>
2019-05-03 12:16:35 -0300

    functioning

---
+++ NBCC-WMAD-RWProps2/Car.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NBCC_WMAD_Console
+{
+    public class Car
+    {
+        private int? speed;//?means it is nullable.
+        private Nullable<decimal> price;//same as decimal?
+        private int numberOfDoors;
+        private string carType;
+        
+        public int? Year { get; set; }//auto properties also work with nullable
+
+        public int? Speed
+        {
+            get
+            {
+                return speed;
+            }
+            set
+            {
+                speed = value;
+            }
+        }
+        public decimal? Price
+        {
+            get { return price; }
+            set { price = value; }
+        }
+
+        public int NumberOfDoors
+        { get { return numberOfDoors; }
+            set {
+                //ensure the value of 10
+                if (value < 10)
+                {
+                    numberOfDoors = value;
+                }
+            }
+        }
+        public string CarType { get; set; }
+        
+    }
+
+}
--- NBCC-WMAD-RWProps2/Program.cs
+++ NBCC-WMAD-RWProps2/Program.cs
@@ -15,7 +15,8 @@ namespace NBCC_WMAD_Console
         private static void LoadMenu()
         {
             Menu.menuDictionary.Add(0, "Exit");
-            Menu.menuDictionary.Add(1, "About the Console");            
+            Menu.menuDictionary.Add(1, "About the Console");
+            Menu.menuDictionary.Add(2, "Car Builder");
         }
 
         static void Main(string[] args)
@@ -33,18 +34,63 @@ namespace NBCC_WMAD_Console
                         AboutThisApp();
                         break;
                     //Add more options in the menu here
+                    case 2:
+                        CarBuilder();
+                        break;
                 }
             }
 
             Out.P("Press any key to exit.");
             Console.ReadLine();
         }
-                
+
+
         #region [My Functionality]
 
         /*
          * My methods will be added here for executing functionality within the console
          */
+         //store the created cars in a List
+        static List<Car> myCars = new List<Car>();
+
+        private static void CarBuilder()
+        {
+            string userCarType = In.GetString("Provide a car type (ie Car, Truck, Van: \n");
+            int userNumberOfDoors = In.GetInt("Provide the number of doors: \n");
+            int userCarSpeed = In.GetInt("Provide the car speed: ");
+
+            Car car = new Car();
+
+            car.CarType = userCarType;
+            car.NumberOfDoors = userNumberOfDoors;
+            car.Speed = userCarSpeed;
+
+            Out.P("------------- \n");
+
+            Out.P($"The Car created is: \nType: {car.CarType} \nNumber of doors: {car.NumberOfDoors} \n" +
+                $"Speed: {car.Speed}\nPrice: {car.Price.GetValueOrDefault()}\n");
+
+            Out.P("------------- \n");
+
+            myCars.Add(car);
+
+            Out.P($"I have {myCars.Count} cars in my list.");
+
+            string entry = In.GetString("Do you want to list all cars? y or n");
+            if(entry == "y")
+            {
+                for(int i = 0; i<myCars.Count; i++)
+                {
+                    Out.P($"Car number: {i + 1} \nCar Type: {myCars[i].CarType}\nNumber of doors: {myCars[i].NumberOfDoors}\n" +
+                        $"Speed: {myCars[i].Speed}\nPrice: {myCars[i].Price.GetValueOrDefault().ToString("c")}\n");
+                }
+            }
+            else
+            {
+                return;
+            }
+
+        }
 
         #endregion
 
